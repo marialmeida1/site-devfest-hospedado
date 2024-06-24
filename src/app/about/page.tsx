b39b3a5822c353bef0f9457b5e833ff1dc5c98f7
marialmeida1 <marianaalmeidafga@gmail.com>
2024-06-24 15:12:01 -0300

    feat: criando padrão tailwind

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,8 @@
 export default function About() {
-   return <p>Página sobre o que é o evento</p>;
+   return (
+      <div className="container">
+         <h1 className="">Página Page</h1>
+         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur, reprehenderit! Aspernatur veniam error perspiciatis maiores odit facere sed est explicabo animi sit ipsam, mollitia illum amet, quis accusamus eos optio?</p>
+      </div>
+   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,32 @@ import type {Config} from "tailwindcss";
 const config: Config = {
    content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            blue: "#4285F4",
+            "blue-dark": "#254272",
+            "blue-light": "#9AC0FF",
+            red: "#EA4335",
+            "red-dark": "#811C13",
+            "red-light": "#FA8E85",
+            green: "#0F9D58",
+            "green-dark": "#0E7B46",
+            "green-light": "#71FFBA",
+            yellow: "#FBBC04",
+            "yellow-dark": "#A58117",
+            "yellow-light": "#FFDB74",
+         },
+         container: {
+            padding: "1rem",
+            center: true,
+            screens: {
+               sm: "480px",
+               md: "768px",
+               lg: "976px",
+               xl: "1440px",
+            },
+         },
+      },
    },
    plugins: [],
 };
