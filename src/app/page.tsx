9d62a290ed14387744e9a337e528f79d09dd95c6
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-07-26 19:14:52 -0300

    chore: addition of the sponsors' carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import CountdownTimer from "./components/home/CountdownTimer";
 import HomeButton from "./components/home/HomeButton";
 import SpeakerCarousel from "./components/home/SpeakerCarousel";
 import QuestionAccordion from "./components/home/QuestionAccordion";
+import SponsorCarousel from "./components/home/SponsorCarousel";
 
 export default function Home() {
    return (
@@ -67,6 +68,12 @@ export default function Home() {
                answer="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             />
          </section>
+         <div className="bg-[#F0F0F0]">
+            <section className="container pt-8 pb-16">
+               <SectionTitle align="text-center">Palestrantes</SectionTitle>
+               <SponsorCarousel />
+            </section>
+         </div>
       </>
    );
 }
